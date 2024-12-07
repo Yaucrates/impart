d97a0bd7cdb008108b8fd499a3ea7bc9862e4d6b
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-07 03:47:42 -0500

    Updated Impart

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Header from "$lib/components/navigation/Header.svelte";
+    let { children } = $props();
+</script>
+
+<div class="h-full flex flex-col">
+    <Header />
+    <div class="flex-grow">
+        {@render children()}
+    </div>
+</div>
