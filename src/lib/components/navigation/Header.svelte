0b47b105d4fe6946bb7da1867eb80c2539662c5d
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-01-07 01:19:16 -0500

    Updated Header, Removed Unneed Pages

--- src/lib/components/navigation/Header.svelte
+++ src/lib/components/navigation/Header.svelte
@@ -3,15 +3,14 @@
 </script>
 
 <header class="pt-6 p-10 flex justify-between items-center">
-    <div class="gap-10 flex items-center">
+    <div class="gap-8 flex items-center">
         <a href="/"><ImpartNameLogo scale={0.15} /></a>
-        <button class="text-neutral-200 hover:text-white text-lg transition-colors duration-300">Courses</button>
-        <a class="text-neutral-200 hover:text-white text-lg transition-colors duration-300" href="/mission">Our Mission</a>
+        <a class="font-medium text-white" href="/">Home</a>
+        <a class="font-medium text-white" href="/courses">Courses</a>
+        <a class="font-medium text-white" href="/mission">Our Mission</a>
     </div>
     <div class="gap-8 flex items-center">
-        <a href="/login" class="text-neutral-200 hover:text-white text-lg transition-colors duration-300">Login</a>
-        <div class="px-4 py-2 rounded-3xl cursor-not-allowed bg-blue-600 hover:bg-blue-700 text-white text-lg transition-colors duration-300">
-            Partner with Us
-        </div>
+        <div class="font-medium text-white cursor-not-allowed">Log In</div>
+        <div class="px-4 py-2 font-medium rounded-full text-white cursor-not-allowed bg-blue-600 hover:bg-blue-700 transition-colors duration-300">Partner with Us</div>
     </div>
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,7 @@
 	<title>Impart | Online Asynchronous Higher Education</title>
 	<meta name="description" content="Impart is the best way of learning. All your classes are in one place accessible at anytime anywhere." />
 </svelte:head>
+
+<div class="flex">
+    
+</div>
