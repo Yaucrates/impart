bb87d982c50623a35e7632deab41edbdbde8178b
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-01-08 18:08:24 -0500

    Fixed Footer

--- src/lib/components/navigation/Footer.svelte
+++ src/lib/components/navigation/Footer.svelte
@@ -6,11 +6,11 @@
     <ImpartNameLogo scale={0.12} />
     <p class="text-neutral-300 text-sm">We make education accessible to anyone anywhere anytime.</p>
     <div class="gap-4 flex justify-center items-center">
-        <a class="text-sm text-neutral-300 hidden lg:block" href="/">Home</a>
-        <a class="text-sm text-neutral-300 hidden lg:block" href="/courses">Courses</a>
-        <a class="text-sm text-neutral-300 hidden lg:block" href="/mission">Our Mission</a>
-        <div class="text-sm text-neutral-300 cursor-not-allowed hidden lg:block">Log In</div>
-        <div class="w-fit px-4 py-2 text-sm rounded-full text-white bg-blue-600 hover:bg-blue-700 transition-colors duration-300 cursor-not-allowed hidden lg:block">Partner with Us</div>
+        <a class="text-sm text-neutral-300" href="/">Home</a>
+        <a class="text-sm text-neutral-300" href="/courses">Courses</a>
+        <a class="text-sm text-neutral-300" href="/mission">Our Mission</a>
+        <div class="text-sm text-neutral-300 cursor-not-allowed">Log In</div>
+        <div class="w-fit px-4 py-2 text-sm rounded-full text-white bg-blue-600 hover:bg-blue-700 transition-colors duration-300 cursor-not-allowed">Partner with Us</div>
     </div>
     <p class="text-neutral-300 text-sm">Copyright © 2025 - All rights reserved</p>
 </footer>
