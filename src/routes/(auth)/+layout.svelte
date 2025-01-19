a9fe1ffdf4d5255d826310a90f68158df28b8f4a
Yaucrates <abdulhusseinyousifh@gmail.com>
2025-01-19 03:06:59 -0500

    Auth Change + Course Variable Rename

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -3,11 +3,12 @@
     import type { LayoutData } from './$types';
     import ImpartNameLogo from '$lib/components/logos/ImpartNameLogo.svelte';
     import '../../app.css';
+    import Footer from '$lib/components/navigation/Footer.svelte';
 
     let { data, children }: { data: LayoutData, children: Snippet } = $props();
 </script>
 
-<main class="flex flex-col items-center">
+<main class="min-h-screen pb-20 flex flex-col items-center">
     <header class="w-full max-w-screen-lg pb-20 p-8 gap-6 flex items-center">
         <a href="/"><ImpartNameLogo scale={0.15}/></a>
         <hr class="w-full h-px bg-white"/>
@@ -16,3 +17,4 @@
 
     {@render children()}
 </main>
+<Footer />
--- src/routes/(main)/courses/single-variable-calculus/+layout.svelte
+++ src/routes/(main)/courses/single-variable-calculus/+layout.svelte
@@ -5,7 +5,7 @@
 
     let { children }: { children: Snippet } = $props();
 
-    let chapterNumber = $state(0);
+    let currentChapterNumber = $state(0);
     let toggleDropdown = $state(false);
 
     const chapters = [
@@ -33,8 +33,19 @@
             number: 5,
             name: "Coordinate Systems and Infinite Series",
         }
-    ]
+    ];
+
+    const lessons = [
+        {
+            chapterNumber: 0,
+        }
+    ];
 </script>
+
+<svelte:head>
+	<title>Impart | Single Variable Calculus</title>
+	<meta name="description" content="This course covers differentiation and integration of functions of one variable, and concludes with a brief discussion of infinite series." />
+</svelte:head>
 
 <div class="h-firstPage flex">
     <div class="p-6 w-80 border-r gap-5 flex flex-col border-neutral-700 overflow-y-scroll">
@@ -45,12 +56,12 @@
         <p class="text-neutral-300">You've completed <span class="text-red-500">7%</span> of the course and currently have a grade of <span class="text-green-500">A+</span>.</p>
         <div class="flex flex-col rounded-lg overflow-hidden relative">
             <button onclick={() => { toggleDropdown = !toggleDropdown; }} class="px-4 py-2 gap-4 flex bg-impart-600 hover:bg-impart-700 duration-300 transition-colors">
-                <div class="text-neutral-100 text-start truncate">Chapter {chapters[chapterNumber].number}: {chapters[chapterNumber].name}</div>
+                <div class="text-neutral-100 text-start truncate">Chapter {chapters[currentChapterNumber].number}: {chapters[currentChapterNumber].name}</div>
                 <ChevronRight className="{toggleDropdown ? "rotate-90" : "rotate-0"} stroke-white duration-100 transition-all" />
             </button>
             <div class="flex flex-col w-full bg-impart-600 overflow-y-scroll {toggleDropdown || "hidden"}">
                 {#each chapters as chapter, i}
-                    <button onclick={() => { chapterNumber = i; toggleDropdown = false; } } class="px-4 py-2 gap-4 text-neutral-100 text-start truncate bg-impart-600 hover:bg-impart-700  duration-300 transition-colors {i === chapterNumber && "hidden"}">
+                    <button onclick={() => { currentChapterNumber = i; toggleDropdown = false; } } class="px-4 py-2 gap-4 text-neutral-100 text-start truncate bg-impart-600 hover:bg-impart-700  duration-300 transition-colors {i === currentChapterNumber && "hidden"}">
                         Chapter {chapter.number}: {chapter.name}
                     </button>
                 {/each}
